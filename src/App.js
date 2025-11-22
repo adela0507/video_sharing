5c8e1b98b77b8df05b7b88d44006ff85480f950b
adela miclea <adelamiclea709@yahoo.com>
2025-11-22 13:02:16 +0200

    material/ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import youtube from './api/youtube'
+import {Grid} from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Grid style={{justifyContent:"center"}} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>{/*SearchBar*/}</Grid>
+          <Grid item xs={8}>{/*VideoDetail*/}</Grid>
+          <Grid item xs={4}>{/*VideoList*/}</Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
