4f1c243b87a54ddc9f32ed4acb475f42298fa24c
adela miclea <adelamiclea709@yahoo.com>
2025-11-23 20:04:35 +0200

    searcher component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import './App.css';
-import youtube from './api/youtube'
-import {Grid} from '@material-ui/core'
-import youtube from './api/youtube';
+import React from "react";
+import { Grid } from "@mui/material";
+import SearchBar from "./components/SearchBar";
+import youtube from "./api/youtube";
 
 function App() {
+  const handleSubmit = async (searchTerm) => {
+    try {
+      const response = await youtube.get("search", {
+        params: {
+          part: "snippet",
+          maxResults: 5,
+          key: "AIzaSyAcP35e4NT8m3Xuk9QGqF3yNMlbOaAKQJM",
+          q: searchTerm,
+        },
+      });
+      console.log(response.data.items); 
+    } catch (error) {
+      console.error("Error fetching videos:", error);
+    }
+  };
+
   return (
-    <Grid style={{justifyContent:"center"}} container spacing={10}>
+    <Grid container justifyContent="center" spacing={2} style={{ padding: 20 }}>
       <Grid item xs={11}>
-        <Grid container spacing={10}>
-          <Grid item xs={12}>{/*SearchBar*/}</Grid>
-          <Grid item xs={8}>{/*VideoDetail*/}</Grid>
-          <Grid item xs={4}>{/*VideoList*/}</Grid>
-        </Grid>
+        <SearchBar onSubmit={handleSubmit} />
+      </Grid>
+      <Grid item xs={8}>
+        {/* VideoDetail */}
+      </Grid>
+      <Grid item xs={4}>
+        {/* VideoList */}
       </Grid>
     </Grid>
   );
